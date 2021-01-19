5fc28f83c0499836f6b2534a9c45bf2eb83dfbda
Eric Kelley <hello@ericmkelley.com>
2021-01-19 15:29:19 -0500

    animations

--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -13,23 +13,15 @@
       </div>
       <div class="column is-4 pokemon-wheel">
         <div class="pokemon-wheel--container pxl-border">
-          <div class="previous">
+          <div
+            class="previous"
+            v-for="pokemon in pokemonNames"
+            :key="pokemon.id"
+          >
             <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/1.png"
-              width="100%"
-              alt=""
-            />
-          </div>
-          <div class="current">
-            <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/4.png"
-              width="100%"
-              alt=""
-            />
-          </div>
-          <div class="next">
-            <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/7.png"
+              :id="pokemon.id"
+              :src="getSpriteURL(pokemon.id)"
+              :class="{ 'is-first': pokemon.id === 1 }"
               width="100%"
               alt=""
             />
@@ -38,8 +30,14 @@
       </div>
       <div class="column scrollable-list">
         <div class="scrollable-list--container">
-          <ul v-if="pokemonNames">
-            <li v-for="pokemon in pokemonNames" :key="pokemon.id">
+          <ul id="pokemonList" v-if="pokemonNames">
+            <li
+              v-for="pokemon in pokemonNames"
+              :key="pokemon.id"
+              @click="setActive(pokemon.id)"
+              :id="getListItemID(pokemon.id)"
+              :class="{ 'is-first is-active': pokemon.id === 1 }"
+            >
               <span>No</span>{{ pad(pokemon.id, 3) }} {{ pokemon.name }}
             </li>
           </ul>
@@ -83,6 +81,24 @@ export default {
         str = "0" + str;
       }
       return str;
+    },
+    getSpriteURL(id) {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/${id}.png`;
+    },
+    getListItemID(id) {
+      return `pokemon-${id}`;
+    },
+    setActive(id) {
+      const activeSprite = document.getElementById(id);
+      const activeListItem = document.getElementById(`pokemon-${id}`);
+      activeListItem.scrollIntoView({ block: "center" });
+      activeSprite.scrollIntoView({ behavior: "smooth", block: "center" });
+      this.toggleClasses(activeListItem);
+    },
+    toggleClasses(newActiveItem) {
+      const oldActive = document.getElementsByClassName("is-active")[0];
+      oldActive.classList.toggle("is-active");
+      newActiveItem.classList.toggle("is-active");
     },
   },
 };
@@ -150,7 +166,7 @@ export default {
   border-radius: 10px;
   height: 500px;
   width: 100%;
-  overflow: hidden;
+  overflow-y: scroll;
   background: linear-gradient(
       0deg,
       #9f9ea3 0%,
@@ -159,12 +175,24 @@ export default {
       #9f9ea3 100%
     ),
     white;
+
+  &::-webkit-scrollbar-track {
+    display: none;
+  }
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    display: none;
+  }
   img {
     image-rendering: pixelated;
     image-rendering: -moz-crisp-edges;
     image-rendering: crisp-edges;
-    width: 250px;
-    margin-top: -3rem;
+    width: 200px;
+    // margin-top: -3rem;
   }
 }
 .scrollable-list {
@@ -186,19 +214,26 @@ export default {
   color: rgb(75, 75, 75);
   text-shadow: 2px 2px #e6e6e6;
   text-transform: uppercase;
+  user-select: none;
+  cursor: pointer;
   span {
+    margin-left: 0.5rem;
     text-transform: none;
     font-weight: bold;
     color: rgb(34, 34, 34);
   }
+  .is-active {
+    background: white;
+    border-radius: 20px;
+  }
 
   &::-webkit-scrollbar-track {
-    background-color: #ffbf54;
+    background-color: transparent;
   }
 
   &::-webkit-scrollbar {
-    width: 13px;
-    background-color: #ffbf54;
+    width: 5px;
+    background-color: transparent;
   }
 
   &::-webkit-scrollbar-thumb {
@@ -207,5 +242,8 @@ export default {
     border-bottom-left-radius: 4px;
     margin-right: 1rem;
   }
+}
+.is-first {
+  margin-top: 80%;
 }
 </style>
